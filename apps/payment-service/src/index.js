ad896a1201e5dfc3b44d2544db3abe666db7cd94
tuannguyen <tuannguyen@everfit.io>
2026-01-13 10:13:00 +0700

    clean: remove cmt

--- apps/payment-service/src/index.js
+++ apps/payment-service/src/index.js
@@ -2,6 +2,5 @@ const startGrpcServer = require('./grpc-server');
 
 console.log('Payment Service is starting...');
 
-// Trong thực tế sẽ có kết nối DB và Express REST API ở đây nếu cần
-// Ở đây chúng ta chỉ chạy gRPC Server để demo
+// Connect to MongoDB and start gRPC server
 startGrpcServer();
